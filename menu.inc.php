a16658b5fb58fc561dd7d21fd6f6964ff9dfd683
Marco Mazzetti <marcomazzo99@gmail.com>
2023-11-06 09:42:20 +0100

    test vari

---
+++ menu.inc.php
@@ -0,0 +1,12 @@
+<div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+      </div>
+  
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" id="scrivi" class="nav-link px-2">Scrivi</a></li>
+        <li><a href="verifica.html" class="nav-link px-2">Verifica</a></li>
+        <li><a href="#" class="nav-link px-2">Informazioni</a></li>
+      </ul>
