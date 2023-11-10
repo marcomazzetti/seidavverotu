5816305d1084d2a24bf2b4e7e3eaae6106ccc98f
Marco Mazzetti <marcomazzo99@gmail.com>
2023-11-10 18:19:26 +0100

    media fatta!

--- scrivi.js
+++ scrivi.js
@@ -21,30 +21,48 @@ checkWord.innerHTML = frasee;
 //qui inizio a concentrarmi su cosa scrive l'utente
 let checkInput = document.querySelector('#check-input');
 
+const resetButton = document.querySelector('#reset');
+
 let firstInput, lastInput, diffArray = []; //dichiaro 3 variabili
-checkInput.onkeypress = (e) => {    //ogni volta che viene premuto un tasto si attiva l'ascoltatore di eventi
+
+checkInput.onkeydown = (e) => {    
+  if (e.keyCode === 8 || e.keyCode === 46) { //così facendo l'utente non può cancellare
+    e.preventDefault();
+  }
+}
+checkInput.onkeyup = (e) => {    //ogni volta che viene rilasciato un tasto
   if (!firstInput) { //se first input non ha un valore (cioè se è la prima volta che lo premo)
     firstInput = e.timeStamp; //first input diventa quello che viene ascoltato
     lastInput = firstInput;  // last input anche
   }
   const diff = e.timeStamp - lastInput; //calcola differenza di tempo tra la pressione del tasto corrente e la pressione del tasto precedente
   diffArray.push({ timeStamp: e.timeStamp, diff, key: e.key }); //aggiunge  l'oggetto che contiene le informazioni sulla pressione del tasto corrente all'array diffArray.
-  console.log("La differenza in secondi è : " + diff / 1000 + "s", diffArray)
+  console.log("La differenza in secondi è : " + diff / 1000 + "s", diffArray);
   lastInput = e.timeStamp; //aggiorno la variabile last input al timestamp corrente
   console.log("checkInput: ", checkInput.value);
   console.log("checkWord: ", checkWord.textContent);
   if (checkInput.value === checkWord.textContent) {
     console.log("Va!");
     checkInput.disabled = true;
+    resetButton.disabled = true;
+    resetButton.textContent = "Frase inserita correttamente!";
+    //adesso devo iniziare a ragionare sull'array per poter fare la media
+    let somma = 0;
+    console.log(diffArray);
+    for (const key of Object.keys(diffArray)) {
+      somma += diffArray[key].diff;
+    }
+    let media = somma/ (diffArray.length - 1);
+    console.log("La media in secondi è : " + media/1000 + "s");
   }
 }
-
 // Quando si preme il bottone "Cliccami!",
-document.querySelector('#reset').addEventListener('click', () => {
+resetButton.addEventListener('click', () => {
   diffArray = []; //questo va bene 
   checkInput.value = '';
-  firstInput = null;
-
+  firstInput = null
 
   /*window.location = 'verifica.php';*/
 });
+
+//adesso devo iniziare a ragionare sull'array per poter fare la media
